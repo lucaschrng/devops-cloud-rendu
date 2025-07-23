e4fe89064d372cae36531e07ef7b969d122e4eb2
Lucas Charoing <charoing.lucas@gmail.com>
2025-07-23 15:17:04 +0200

    fix: add polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+      // Polyfills for specific Node.js globals and modules
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
