3aaccd643a8f80453e5ab05cbe30aad3916cc09f
Lucas Charoing <charoing.lucas@gmail.com>
2025-07-23 11:09:04 +0200

    feat: implement AWS Amplify authentication with Vue router integration

---
+++ src/pages/Auth.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import { Amplify } from 'aws-amplify';
+  import awsconfig from '../amplifyconfiguration.json';
+
+  Amplify.configure(awsconfig);
+</script>
+
+<template>
+  <Authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </Authenticator>
+</template>
